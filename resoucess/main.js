cf479d99f1b06cbce523e0d7cf3f1ec0b753faed
Filmag-kag <60930977+Filmag-kag@users.noreply.github.com>
2020-02-13 15:58:20 +0100

    animated button

--- resoucess/main.js
+++ resoucess/main.js
@@ -1,6 +1,7 @@
-const navButton = document.querySelector('.menu');
+const navButton = document.querySelector('.menubutton');
 const toggleNav = () => {
   const navCon = document.querySelector('.nav-container');
   navCon.classList.toggle('navtoggled');
+  navButton.classList.toggle('menu--open');
 }
 navButton.addEventListener('click',toggleNav);
